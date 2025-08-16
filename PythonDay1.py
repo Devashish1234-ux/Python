ba00aa158e9cc03bd67cfdbbeb7abd80aaf3c2c0
evashish1234-ux <devashishbogati04@gmail.com>
2025-08-16 14:56:09 +0545

    python

---
+++ PythonDay1.py
@@ -0,0 +1,5 @@
+from colorama import Fore, Style
+
+print(Fore.RED + "Hello, World!" + Style.RESET_ALL)
+print(Fore.GREEN + "This is a green message." + Style.RESET_ALL)
+print(Fore.BLUE + "And this is blue." + Style.RESET_ALL)    
